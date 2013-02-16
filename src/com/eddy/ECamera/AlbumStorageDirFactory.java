e7270b3c20b508d52a70746b2fbd3982d51f29ea
sniciq <sniciq@gmail.com>
2013-02-16 17:02:16 +0800

    init

---
+++ src/com/eddy/ECamera/AlbumStorageDirFactory.java
@@ -0,0 +1,7 @@
+package com.eddy.ECamera;
+
+import java.io.File;
+
+public abstract class AlbumStorageDirFactory {
+	public abstract File getAlbumStorageDir(String albumName);
+}
---
+++ src/com/eddy/ECamera/MainActivity.java
@@ -0,0 +1,212 @@
+package com.eddy.ECamera;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.VideoView;
+
+public class MainActivity extends Activity {
+	
+	private ImageView mImageView;
+	private Bitmap mImageBitmap;
+	private VideoView mVideoView;
+	private Uri mVideoUri;
+	
+	private static final int ACTION_TAKE_PHOTO_B = 1;
+	private static final int ACTION_TAKE_PHOTO_S = 2;
+	private static final int ACTION_TAKE_VIDEO = 3;
+	
+	private String mCurrentPhotoPath;
+	private static final String JPEG_FILE_PREFIX = "IMG_";
+	private static final String JPEG_FILE_SUFFIX = ".jpg";
+	
+	private AlbumStorageDirFactory mAlbumStorageDirFactory = null;
+	
+//	Button.OnClickListener mTakePicOnClickListener = 
+//			new Button.OnClickListener() {
+//			@Override
+//			public void onClick(View v) {
+//				dispatchTakePictureIntent(ACTION_TAKE_PHOTO_B);
+//			}
+//		};
+	
+	Button.OnClickListener mTakePicOnClickListener =  new Button.OnClickListener() {
+		@Override
+		public void onClick(View arg0) {
+			dispatchTakePictureIntent(ACTION_TAKE_PHOTO_B);
+		}
+	};
+	
+	private void dispatchTakePictureIntent(int actionCode) {
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		switch (actionCode) {
+		case ACTION_TAKE_PHOTO_B:
+			File f = null;
+			try {
+				f = setUpPhotoFile();
+			} catch (Exception e) {
+				e.printStackTrace();
+				f = null;
+			}
+			break;
+		default:
+			break;
+		}
+		startActivityForResult(takePictureIntent, actionCode);
+	}
+
+	private File setUpPhotoFile() throws IOException {
+		File f = createImageFile();
+		mCurrentPhotoPath = f.getAbsolutePath();
+		return null;
+	}
+
+	private File createImageFile() throws IOException {
+		String timeStamp = new SimpleDateFormat("yyyyMMdd-HHmmss", Locale.US).format(new Date());
+		String imageFileName = JPEG_FILE_PREFIX + timeStamp + "_";
+		File albumF = getAlbumDir();
+		File imageF = File.createTempFile(imageFileName, JPEG_FILE_SUFFIX, albumF);
+		return imageF;
+	}
+
+	private File getAlbumDir() {
+		File storageDir = null;
+		if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+			storageDir = mAlbumStorageDirFactory.getAlbumStorageDir(getAlbumName());
+			if(storageDir != null) {
+				if(!storageDir.mkdirs()) {
+					if (! storageDir.exists()){
+						Log.d("CameraSample", "failed to create directory");
+						return null;
+					}
+				}
+			}
+		} else {
+			Log.v(getString(R.string.app_name), "External storage is not mounted READ/WRITE.");
+		}
+		return storageDir;
+	}
+
+	private String getAlbumName() {
+		return getString(R.string.album_name);
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mImageView = (ImageView) findViewById(R.id.imageView1);
+		mVideoView = (VideoView) findViewById(R.id.videoView1);
+		mImageBitmap = null;
+		mVideoUri = null;
+		
+		Button picBtn = (Button) findViewById(R.id.btnIntend);
+		setBtnListenerOrDisable(picBtn, mTakePicOnClickListener, MediaStore.ACTION_IMAGE_CAPTURE);
+		
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO) {
+			mAlbumStorageDirFactory = new FroyoAlbumDirFactory();
+		} else {
+			mAlbumStorageDirFactory = new BaseAlbumDirFactory();
+		}
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+			case ACTION_TAKE_PHOTO_B: {
+				if (resultCode == RESULT_OK) {
+					handleBigCameraPhoto();
+				}
+				break;
+			}
+		}
+	}
+	
+	private void handleBigCameraPhoto() {
+		if(mCurrentPhotoPath != null) {
+			setPic();
+			galleryAddPic();
+			mCurrentPhotoPath = null;
+		}
+	}
+
+	private void galleryAddPic() {
+		Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+		File f = new File(mCurrentPhotoPath);
+	    Uri contentUri = Uri.fromFile(f);
+	    mediaScanIntent.setData(contentUri);
+	    this.sendBroadcast(mediaScanIntent);
+	}
+
+	private void setPic() {
+		int targetW = mImageView.getWidth();
+		int targetH = mImageView.getHeight();
+		BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+		bmOptions.inJustDecodeBounds = false;
+		BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+		int photoW = bmOptions.outWidth;
+		int photoH = bmOptions.outHeight;
+		
+		int scaleFactor = 1;
+		if ((targetW > 0) || (targetH > 0)) {
+			scaleFactor = Math.min(photoW/targetW, photoH/targetH);	
+		}
+		
+		bmOptions.inJustDecodeBounds = false;
+		bmOptions.inSampleSize = scaleFactor;
+		bmOptions.inPurgeable = true;
+		
+		Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+		
+		mImageView.setImageBitmap(bitmap);
+		mVideoUri = null;
+		mImageView.setVisibility(View.VISIBLE);
+		mVideoView.setVisibility(View.INVISIBLE);
+	}
+
+	public static boolean isItentAvailable(Context context, String action) {
+		final PackageManager packageManager = context.getPackageManager();
+		final Intent intent = new Intent(action);
+		List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+		return list.size() > 0;
+	}
+
+	private void setBtnListenerOrDisable(Button btn, Button.OnClickListener onClickListener, String intentName) {
+		if(isItentAvailable(this, intentName)) {
+			btn.setOnClickListener(onClickListener);
+		} else {
+			btn.setText(getText(R.string.cannot).toString() + " " + btn.getText());
+			btn.setClickable(false);
+		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
