0167fdd20432b06f2772f3083106c414aac8eb46
sniciq <sniciq@gmail.com>
2013-02-18 12:04:36 +0800

    init

--- src/com/eddy/ECamera/MainActivity.java
+++ src/com/eddy/ECamera/MainActivity.java
@@ -24,39 +24,70 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends Activity {
-	
+
 	private ImageView mImageView;
 	private Bitmap mImageBitmap;
 	private VideoView mVideoView;
 	private Uri mVideoUri;
-	
+
 	private static final int ACTION_TAKE_PHOTO_B = 1;
 	private static final int ACTION_TAKE_PHOTO_S = 2;
 	private static final int ACTION_TAKE_VIDEO = 3;
-	
+
 	private String mCurrentPhotoPath;
 	private static final String JPEG_FILE_PREFIX = "IMG_";
 	private static final String JPEG_FILE_SUFFIX = ".jpg";
-	
+	private static final String VDO_FILE_PREFIX = "VDO_";
+	private static final String VDO_FILE_SUFFIX = ".3gp";
+
 	private AlbumStorageDirFactory mAlbumStorageDirFactory = null;
-	
-//	Button.OnClickListener mTakePicOnClickListener = 
-//			new Button.OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				dispatchTakePictureIntent(ACTION_TAKE_PHOTO_B);
-//			}
-//		};
-	
-	Button.OnClickListener mTakePicOnClickListener =  new Button.OnClickListener() {
+
+	private static final String BITMAP_STORAGE_KEY = "viewbitmap";
+	private static final String IMAGEVIEW_VISIBILITY_STORAGE_KEY = "imageviewvisibility";
+
+	private static final String VIDEO_STORAGE_KEY = "viewvideo";
+	private static final String VIDEOVIEW_VISIBILITY_STORAGE_KEY = "videoviewvisibility";
+
+	Button.OnClickListener mTakePicOnClickListener = new Button.OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
 			dispatchTakePictureIntent(ACTION_TAKE_PHOTO_B);
 		}
 	};
+
+	Button.OnClickListener mTakeSmallPicOnClickListener = new Button.OnClickListener() {
+		@Override
+		public void onClick(View arg0) {
+			dispatchTakePictureIntent(ACTION_TAKE_PHOTO_S);
+		}
+	};
+	
+	Button.OnClickListener mTakeVidOnClickListener = new Button.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			dispatchTakeVideoIntent();
+		}
+	};
+
+	private void dispatchTakeVideoIntent() {
+		try {
+			Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+			String timeStamp = new SimpleDateFormat("yyyyMMdd-HHmmss", Locale.US).format(new Date());
+			String imageFileName = VDO_FILE_PREFIX + timeStamp + "_";
+			File albumF = getAlbumDir();
+			File vf = File.createTempFile(imageFileName, VDO_FILE_SUFFIX, albumF);
+			mCurrentPhotoPath = vf.getAbsolutePath();
+			takeVideoIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(vf));
+			startActivityForResult(takeVideoIntent, ACTION_TAKE_VIDEO);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	private void dispatchTakePictureIntent(int actionCode) {
 		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -65,6 +96,8 @@ public class MainActivity extends Activity {
 			File f = null;
 			try {
 				f = setUpPhotoFile();
+				mCurrentPhotoPath = f.getAbsolutePath();
+				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
 			} catch (Exception e) {
 				e.printStackTrace();
 				f = null;
@@ -75,11 +108,35 @@ public class MainActivity extends Activity {
 		}
 		startActivityForResult(takePictureIntent, actionCode);
 	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+		case ACTION_TAKE_PHOTO_B: {
+			if (resultCode == RESULT_OK) {
+				handleBigCameraPhoto();
+			}
+			break;
+		}
+		case ACTION_TAKE_PHOTO_S: {
+			if (resultCode == RESULT_OK) {
+				handleSmallCameraPhoto(data);
+			}
+			break;
+		}
+		case ACTION_TAKE_VIDEO: {
+			if (resultCode == RESULT_OK) {
+				handleCameraVideo(data);
+			}
+			break;
+		}
+		}
+	}
 
 	private File setUpPhotoFile() throws IOException {
 		File f = createImageFile();
 		mCurrentPhotoPath = f.getAbsolutePath();
-		return null;
+		return f;
 	}
 
 	private File createImageFile() throws IOException {
@@ -92,11 +149,11 @@ public class MainActivity extends Activity {
 
 	private File getAlbumDir() {
 		File storageDir = null;
-		if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+		if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
 			storageDir = mAlbumStorageDirFactory.getAlbumStorageDir(getAlbumName());
-			if(storageDir != null) {
-				if(!storageDir.mkdirs()) {
-					if (! storageDir.exists()){
+			if (storageDir != null) {
+				if (!storageDir.mkdirs()) {
+					if (!storageDir.exists()) {
 						Log.d("CameraSample", "failed to create directory");
 						return null;
 					}
@@ -116,36 +173,49 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		mImageView = (ImageView) findViewById(R.id.imageView1);
 		mVideoView = (VideoView) findViewById(R.id.videoView1);
+		mVideoView.setMediaController(new MediaController(this));
 		mImageBitmap = null;
 		mVideoUri = null;
-		
+
 		Button picBtn = (Button) findViewById(R.id.btnIntend);
 		setBtnListenerOrDisable(picBtn, mTakePicOnClickListener, MediaStore.ACTION_IMAGE_CAPTURE);
+
+		Button sPicBtn = (Button) findViewById(R.id.btnIntendS);
+		setBtnListenerOrDisable(sPicBtn, mTakeSmallPicOnClickListener, MediaStore.ACTION_IMAGE_CAPTURE);
+
+		Button vBtn = (Button) findViewById(R.id.btnIntendV);
+		setBtnListenerOrDisable(vBtn, mTakeVidOnClickListener, MediaStore.ACTION_VIDEO_CAPTURE);
 		
-		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO) {
 			mAlbumStorageDirFactory = new FroyoAlbumDirFactory();
 		} else {
 			mAlbumStorageDirFactory = new BaseAlbumDirFactory();
 		}
 	}
 	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		switch (requestCode) {
-			case ACTION_TAKE_PHOTO_B: {
-				if (resultCode == RESULT_OK) {
-					handleBigCameraPhoto();
-				}
-				break;
-			}
-		}
+	private void handleCameraVideo(Intent data) {
+		mVideoUri = data.getData();
+		mVideoView.setVideoURI(mVideoUri);
+		mImageBitmap = null;
+		mVideoView.setVisibility(View.VISIBLE);
+		mImageView.setVisibility(View.INVISIBLE);
+		galleryAddPic();
 	}
-	
+
+	private void handleSmallCameraPhoto(Intent intentData) {
+		Bundle extras = intentData.getExtras();
+		mImageBitmap = (Bitmap) extras.get("data");
+		mImageView.setImageBitmap(mImageBitmap);
+		mVideoUri = null;
+		mImageView.setVisibility(View.VISIBLE);
+		mVideoView.setVisibility(View.INVISIBLE);
+	}
+
 	private void handleBigCameraPhoto() {
-		if(mCurrentPhotoPath != null) {
+		if (mCurrentPhotoPath != null) {
 			setPic();
 			galleryAddPic();
 			mCurrentPhotoPath = null;
@@ -155,12 +225,14 @@ public class MainActivity extends Activity {
 	private void galleryAddPic() {
 		Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
 		File f = new File(mCurrentPhotoPath);
-	    Uri contentUri = Uri.fromFile(f);
-	    mediaScanIntent.setData(contentUri);
-	    this.sendBroadcast(mediaScanIntent);
+		Uri contentUri = Uri.fromFile(f);
+		mediaScanIntent.setData(contentUri);
+		this.sendBroadcast(mediaScanIntent);
 	}
 
 	private void setPic() {
+		mImageView.setVisibility(View.VISIBLE);
+		mVideoView.setVisibility(View.INVISIBLE);
 		int targetW = mImageView.getWidth();
 		int targetH = mImageView.getHeight();
 		BitmapFactory.Options bmOptions = new BitmapFactory.Options();
@@ -168,22 +240,41 @@ public class MainActivity extends Activity {
 		BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
 		int photoW = bmOptions.outWidth;
 		int photoH = bmOptions.outHeight;
-		
+
 		int scaleFactor = 1;
-		if ((targetW > 0) || (targetH > 0)) {
-			scaleFactor = Math.min(photoW/targetW, photoH/targetH);	
+		if ((targetW != 0) && (targetH != 0)) {
+			scaleFactor = Math.min(photoW / targetW, photoH / targetH);
 		}
-		
+
 		bmOptions.inJustDecodeBounds = false;
 		bmOptions.inSampleSize = scaleFactor;
 		bmOptions.inPurgeable = true;
-		
+
 		Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-		
+
 		mImageView.setImageBitmap(bitmap);
 		mVideoUri = null;
-		mImageView.setVisibility(View.VISIBLE);
-		mVideoView.setVisibility(View.INVISIBLE);
+	}
+
+	// Some lifecycle callbacks so that the image can survive orientation change
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putParcelable(BITMAP_STORAGE_KEY, mImageBitmap);
+		outState.putParcelable(VIDEO_STORAGE_KEY, mVideoUri);
+		outState.putBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY, (mImageBitmap != null));
+		outState.putBoolean(VIDEOVIEW_VISIBILITY_STORAGE_KEY, (mVideoUri != null));
+		super.onSaveInstanceState(outState);
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+		mImageBitmap = savedInstanceState.getParcelable(BITMAP_STORAGE_KEY);
+		mVideoUri = savedInstanceState.getParcelable(VIDEO_STORAGE_KEY);
+		mImageView.setImageBitmap(mImageBitmap);
+		mImageView.setVisibility(savedInstanceState.getBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY) ? ImageView.VISIBLE : ImageView.INVISIBLE);
+		mVideoView.setVideoURI(mVideoUri);
+		mVideoView.setVisibility(savedInstanceState.getBoolean(VIDEOVIEW_VISIBILITY_STORAGE_KEY) ? ImageView.VISIBLE : ImageView.INVISIBLE);
 	}
 
 	public static boolean isItentAvailable(Context context, String action) {
@@ -194,17 +285,16 @@ public class MainActivity extends Activity {
 	}
 
 	private void setBtnListenerOrDisable(Button btn, Button.OnClickListener onClickListener, String intentName) {
-		if(isItentAvailable(this, intentName)) {
+		if (isItentAvailable(this, intentName)) {
 			btn.setOnClickListener(onClickListener);
 		} else {
 			btn.setText(getText(R.string.cannot).toString() + " " + btn.getText());
 			btn.setClickable(false);
 		}
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
